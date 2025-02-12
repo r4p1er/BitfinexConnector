7ea9e059b9de16696ff87fb6feb6f17055ef9569
Alexander Tsyganok <tsyganok2015@gmail.com>
2025-02-12 17:40:32 +0300

    Feat: add given abstractions and models

--- BitfinexConnector.Core/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace BitfinexConnector.Core;
-
-public class Class1
-{
-}
---
+++ BitfinexConnector.Core/Models/Trade.cs
@@ -0,0 +1,34 @@
+namespace BitfinexConnector.Core.Models;
+
+public class Trade
+{
+    /// <summary>
+    /// Валютная пара
+    /// </summary>
+    public string Pair { get; set; } = null!;
+
+    /// <summary>
+    /// Цена трейда
+    /// </summary>
+    public decimal Price { get; set; }
+
+    /// <summary>
+    /// Объем трейда
+    /// </summary>
+    public decimal Amount { get; set; }
+
+    /// <summary>
+    /// Направление (buy/sell)
+    /// </summary>
+    public string Side { get; set; } = null!;
+
+    /// <summary>
+    /// Время трейда
+    /// </summary>
+    public DateTimeOffset Time { get; set; }
+
+    /// <summary>
+    /// Id трейда
+    /// </summary>
+    public string Id { get; set; } = null!;
+}
